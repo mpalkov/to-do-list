8d4a7c394822b66bd1106df31099942fb456e205
Martin Palkov <martin.palkov@gmail.com>
2025-03-12 18:55:20 +0100

    style: add styles for all scenarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
 import './App.css';
 import { ThemeProvider } from './context/ThemeContext';
-import ThemeToggle from './components/ThemeToggle/ThemeToggle';
 import ToDoList from './components/ToDoList/ToDoList';
-// import { ContentProvider } from './context/ContentContext';
-
+import Header from './components/Header/Header';
+import { ContentProvider } from './context/ContentContext';
 function App() {
 
   return (
     <>
-    {/* <ContentProvider> */}
+    <ContentProvider>
     <ThemeProvider>
-      <header>
-        {/* < NavFilter /> */}
-        <nav>
-          <span className='desktop-only'>
-            Ver:
-          </span>
-          <ul>
-            <li><button className='btn'>Todas</button></li>
-            <li><button className='btn'>Completadas</button></li>
-            <li><button className='btn'>Pendientes</button></li>
-          </ul>
-        </nav>
-        <ThemeToggle />
-      </header>
+      <Header />
       <main>
         <ToDoList />
       </main>
     </ThemeProvider>
-    {/* </ContentProvider> */}
+    </ContentProvider>
     </>
   )
 }
